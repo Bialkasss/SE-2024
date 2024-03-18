d25b0a9cd2a898b87feb88b52ce5ef5017081292
Bialkasss <bialkass@gmail.com>
2024-03-18 09:14:58 +0100

    3 changes

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,10 +1,14 @@
+//comment on the beginning of the file
 public class Main {
   int x = 10;
-
+  int y = 25;
+	
   public static void main(String[] args) {
     Main myObj1 = new Main();  // Object 1
     Main myObj2 = new Main();  // Object 2
+    Main myObj3 = new Main();  //Object 3
     System.out.println(myObj1.x);
     System.out.println(myObj2.x);
+    System.out.println(myObj3.y);
   }
 }
